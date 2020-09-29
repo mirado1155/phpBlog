880ebf25c6b2482f436860808e5fc775f4be15d6
mirado1155 <60988929+mirado1155@users.noreply.github.com>
2020-09-29 09:37:45 -0500

    Add files via upload

---
+++ blog/connectVars.php
@@ -0,0 +1,8 @@
+<?php
+    //Define database connection constants
+    define('DB_HOST', '');
+    define('DB_USER', '');
+    define('DB_PWD', '');
+    define('DB_NAME', '');
+    define('TABLE_NAME', '')
+?>
