2a0817f01d562f8902c1fe63499feb993884b1ad
mirado1155 <60988929+mirado1155@users.noreply.github.com>
2020-09-29 07:36:56 -0700

    Delete connectVars.php

--- blog/connectVars.php
+++
@@ -1,8 +0,0 @@
-<?php
-    //Define database connection constants
-    define('DB_HOST', 'localhost');
-    define('DB_USER', '');
-    define('DB_PWD', '');
-    define('DB_NAME', '');
-    define('TABLE_NAME', '')
-?>
