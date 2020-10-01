0d4344c2766f1272891af47cb482f8e5e64f5a22
mirado1155 <60988929+mirado1155@users.noreply.github.com>
2020-10-01 10:27:42 -0500

    increased character limit for titles from 25 to 50

--- blog/create.sql
+++ blog/create.sql
@@ -2,7 +2,7 @@
 DROP TABLE IF EXISTS `blog_entry`;
 CREATE TABLE blog_entry (
     id int(5) PRIMARY KEY AUTO_INCREMENT,
-    title varchar(25) NOT NULL,
+    title varchar(50) NOT NULL,
     entry varchar(1500) NULL,
     entry_date date NOT NULL
 );
